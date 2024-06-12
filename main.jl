b47d02bc755172a7035f3ce565dda0f5978dde9a
narodzi <naerodzi@gmail.com>
2024-06-12 02:17:32 +0200

    wip1

--- main.jl
+++ main.jl
@@ -5,19 +5,18 @@ include("model.jl")
 train_data = MNIST(:train)  
 test_data  = MNIST(split=:test)
 
-x_train = reshape(train_data.features, 28, 28, :)
+x_train = reshape(train_data.features, 28 * 28, :)
 y_train  = Flux.onehotbatch(train_data.targets, 0:9)
 
-x_test = reshape(test_data.features, 28, 28, :)
+x_test = reshape(test_data.features, 28 * 28, :)
 y_test  = Flux.onehotbatch(test_data.targets, 0:9)
 
-
-WW = Variable(randn(64, 64))
-WU = Variable(randn(64, 14*14))
-WV = Variable(randn(1, 64))
-bh = Variable(zeros(64))
-by = Variable(zeros(1))
-h = Variable(zeros(64))
+WW = Variable(randn(64, 64)) # hidden weights Wh
+WU = Variable(randn(64, 14*14)) # input weights Wx
+WV = Variable(randn(10, 64)) # output weights Wa
+bh = Variable(zeros(64)) # hidden biases b
+by = Variable(zeros(10)) # output biases Wa
+h = Variable(zeros(64)) # previous hidden state
 
 rnn = myRNN(WW, WU, WV, bh, by, h)
 
@@ -28,7 +27,7 @@ settings = (;
 )
 
 # Train model
-train(rnn, x_train, y_train', settings.epochs, settings.batch_size, settings.eta)
+train(rnn, x_train, y_train, settings.epochs, settings.batch_size, settings.eta)
 
 # Test model
-test(rnn, x_test, y_test') 
+# test(rnn, x_test, y_test) 
--- model.jl
+++ model.jl
@@ -29,11 +29,12 @@ function update_weights!(graph::Vector, learning_rate::Float64, batch_size::Int6
 end
 
 function build_graph(x::Constant, y::Constant, rnn::myRNN)
-    l1 = mat_mul(rnn.h, rnn.WW)
-    l2 = mat_mul(x, rnn.WU)
-    l3 = sum_op(l1, l2, rnn.by)
-    l4 = tan_h(l3)
-    l5 = dense(l4, rnn.WV)
+    # l1 = mat_mul(rnn.WW, rnn.h)
+    # l2 = mat_mul(rnn.WU, x)
+    # l3 = sum_op(l1, l2, rnn.bh)
+    # l4 = tan_h(l3)
+    l1 = rnnCell(rnn.WU, rnn.WW, rnn.h, rnn.bh, x)
+    l5 = dense(l1, rnn.WV)
     e = cross_entropy_loss(l5, y)
 
 	return topological_sort(e)
@@ -45,27 +46,29 @@ function train(rnn::myRNN, x::Any, y::Any, epochs, batch_size, learining_rate)
     @time for i=1:epochs
 
         epoch_loss = 0.0
-        samples = size(x, 3)
+        samples = size(x, 2)
 
         global correct_prediction = 0
         global cumulative = 0
 
         println("Epoch: ", i)
- 
-        for j=1:samples
 
-            x_train = Constant(x[:, :, j])
-            y_train = Constant(y[j, :])
+        for k=1:196:size(x,1)
+            for j=1:samples        
 
-            graph = build_graph(x_train, y_train, rnn)
-            epoch_loss += forward!(graph)
-            backward!(graph)
 
-            if j % batch_size == 0
-                update_weights!(graph, learining_rate, batch_size)
+                x_train = Constant(x[k:k+195, j])
+                y_train = Constant(y[:, j])
+
+                graph = build_graph(x_train, y_train, rnn)
+                epoch_loss += forward!(graph)
+                backward!(graph)
+
+                if j % batch_size == 0
+                    update_weights!(graph, learining_rate, batch_size)
+                end
             end
-
-        end
+        end 
 
         @printf("   Average loss: %.4f\n", epoch_loss/samples)
         @printf("   Train accuracy: %.4f\n", correct_prediction/cumulative)
